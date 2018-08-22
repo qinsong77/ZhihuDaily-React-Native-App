4bccd9efa098ecc1f39af06a37b3b574e45d8f45
notend <xyqinsong@163.com>
2018-08-22 23:48:07 +0800

    测试windows 提交代码

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-// import App from './src/practice/TodoList/app';  //一个todolist practice
-import App from './src/root'
+import App from './src/practice/TodoList/app';  //一个todolist practice
+// import App from './src/root'
 import {name as appName} from './app.json';
 
 //下面两行是屏蔽警告
